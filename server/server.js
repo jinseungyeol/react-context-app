8e4c210308abd53443e0182aa7eccc9a998f7682
jinseungyeol <tmddufkk@naver.com>
2025-07-25 22:48:49 +0900

    test

--- server/server.js
+++ server/server.js
@@ -3,40 +3,48 @@ const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const port = 4000;
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://react-context-app-txfn.onrender.com'
+  ],
   credentials: true
-}))
+}));
 
 // use middleware to serve static images
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // read data from file
 const travelDataRaw = fs.readFileSync('./travel.json', 'utf-8');
 const travelData = JSON.parse(travelDataRaw);
 
+// 루트 경로 추가
+app.get('/', (req, res) => {
+  res.send('Hello from Express server');
+});
+
 app.get('/products', (req, res) => {
-  res.json(travelData.countries)
-})
+  res.json(travelData.countries);
+});
 
 app.get('/options', (req, res) => {
-  res.json(travelData.options)
-})
+  res.json(travelData.options);
+});
 
 let orderHistory = [];
 
 app.post('/order', (req, res) => {
   const orderNumber = Math.floor(Math.random() * 1000000);
-  let order = {price: req.body.totals.total , orderNumber};
+  let order = { price: req.body.totals.total, orderNumber };
   orderHistory.push(order);
-  res.status(201).json( orderHistory )
-})
+  res.status(201).json(orderHistory);
+});
 
 if (require.main === module) {
-  app.listen(port, () => console.log(`listening on port ${port}`))
+  app.listen(port, () => console.log(`listening on port ${port}`));
 }
 
 module.exports = app;
